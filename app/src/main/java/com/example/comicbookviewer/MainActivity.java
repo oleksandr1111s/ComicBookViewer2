304b05361e16092f49e167229b713914de5b58c7
Zhdan <oleksandrzdanov12@gmail.com>
2026-07-28 20:36:28 +0300

    Add digit

--- app/src/main/java/com/example/comicbookviewer/MainActivity.java
+++ app/src/main/java/com/example/comicbookviewer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.comicbookviewer;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvDisplay;
+    private boolean newNumber = true;
     private ImageView imageComic;
     private Button btnPrevious, btnNext, btnFirst;
 
@@ -27,12 +29,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-
-
-
+        tvDisplay = findViewById(R.id.textView);
+        findViewById(R.id.b1).setOnClickListener(v -> addDigit("1"));
+        findViewById(R.id.b2).setOnClickListener(v -> addDigit("2"));
+        findViewById(R.id.b3).setOnClickListener(v -> addDigit("3"));
+        findViewById(R.id.b4).setOnClickListener(v -> addDigit("4"));
+        findViewById(R.id.b5).setOnClickListener(v -> addDigit("5"));
+        findViewById(R.id.b6).setOnClickListener(v -> addDigit("6"));
+        findViewById(R.id.b7).setOnClickListener(v -> addDigit("7"));
+        findViewById(R.id.b8).setOnClickListener(v -> addDigit("8"));
+        findViewById(R.id.b9).setOnClickListener(v -> addDigit("9"));
+        findViewById(R.id.b0).setOnClickListener(v -> addDigit("0"));
     }
-
-
+    private void addDigit(String digit) {
+        if (newNumber) {
+            tvDisplay.setText(digit);
+            newNumber = false;
+        } else {
+            tvDisplay.append(digit);
+        }
+    }
 }
